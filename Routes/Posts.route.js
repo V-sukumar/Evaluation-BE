01295b517a781bdbd01c1a1f5b79cfef6800ce9f
V-sukumar <thalasuku2001@gmail.com>
2023-07-17 17:31:40 +0530

    done

--- Routes/Posts.route.js
+++ Routes/Posts.route.js
@@ -53,37 +53,23 @@ postRouter.post("/create", async(req,res) => {
 
 postRouter.patch("/update/:id", async (req,res) => {
     try{
-       const userID = req.body.userID;
        const postID = req.params.id;
-       const post = await postModel.find({_id: postID});
-       const userIDinPost = post.userID;
-       if(userID === userIDinPost){
         await postModel.findByIdAndUpdate({_id:postID}, req.body);
-        res.status(200).json({msg: `${post.title} has been updated`})
-       }else{
-        res.status(400).json({msg: "Not Authorized"})
-       }
+        res.status(200).json({"msg": "title has been updated"})
     }catch(err){
-        res.send(err);
+        res.status(400).send(err);
     }
 })
 
 
 postRouter.delete("/delete/:id", async(req,res) => {
-   try{
-    const postId = req.body.id;
-    const userId = req.body.userID;
-    const post = await postModel.find({_id: postId});
-    const userIdinPost = post.userID;
-    if(userId === userIdinPost){
-        await postModel.findByIdAndDelete({_id:postId})
-        res.status(200).json({msg: "Post has been deleted"})
-    }else{
-        res.status(400).json({msg: "Not Authorized"});
-    }
-   }catch(err){
-    res.status(400).json({error:err})
-   }
+    try{
+        const postID = req.params.id;
+         await postModel.findByIdAndDelete({_id:postID}, req.body);
+         res.status(200).json({"msg": "title has been deleted"})
+     }catch(err){
+         res.status(400).send(err);
+     }
 })
 
 
